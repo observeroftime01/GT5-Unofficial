585d0cf9e9f578c7ca7cf08eb14df9760e939fb9
observeroftime02 <observeroftime@shizuki.ca>
2021-07-23 11:12:51 -0700

    added a missing recipe for platinum in the EBF, for whatever reason this isn't showing in game. Mod conflict on my side maybe? Anyways, this addition takes care of that.

--- src/main/java/gregtech/loaders/postload/GT_CustomRecipeLoader.java
+++ src/main/java/gregtech/loaders/postload/GT_CustomRecipeLoader.java
@@ -19,6 +19,7 @@ public class GT_CustomRecipeLoader implements Runnable {
             //Blast Furnace Recipes
             GT_Values.RA.addBlastRecipe(GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Electrum, 1), GT_Utility.getIntegratedCircuit(11), Materials.Nitrogen.getGas(1000), null, GT_OreDictUnificator.get(OrePrefixes.ingot, Materials.Electrum, 1), null, 2000, 480, 2454);
             GT_Values.RA.addBlastRecipe(GT_OreDictUnificator.get(OrePrefixes.dust, TurbineSteel, 1), GT_Utility.getIntegratedCircuit(11), Materials.Argon.getGas(1000), null, GT_OreDictUnificator.get(OrePrefixes.ingot, TurbineSteel, 1), null, 2400, 500000, 9000 );
+            GT_Values.RA.addBlastRecipe(GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Platinum, 1), GT_Utility.getIntegratedCircuit(11), Materials.Nitrogen.getGas(1000), null, GT_OreDictUnificator.get(OrePrefixes.ingot, Materials.Platinum, 1), null, 1200,1900, 3500);
 
             //Mixer Recipes
             GT_Values.RA.addMixerRecipe(GT_OreDictUnificator.get(OrePrefixes.dust, Materials.StainlessSteel, 4), GT_OreDictUnificator.get(OrePrefixes.dust ,Materials.Electrum, 1), GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Carbon, 12), GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Oriharukon, 6), null, null, GT_OreDictUnificator.get(OrePrefixes.dust, TurbineSteel, 11), 4800, 500000);
