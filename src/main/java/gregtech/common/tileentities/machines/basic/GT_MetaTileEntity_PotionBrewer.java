ad3d75260fdce94cba38048e4657020d8d753d72
observeroftime02 <observeroftime@shizuki.ca>
2021-08-18 11:35:10 -0700

    Increased potion brewer capacity by 750L each tier (if my code worked lol)

--- src/main/java/gregtech/common/tileentities/machines/basic/GT_MetaTileEntity_PotionBrewer.java
+++ src/main/java/gregtech/common/tileentities/machines/basic/GT_MetaTileEntity_PotionBrewer.java
@@ -171,6 +171,43 @@ public class GT_MetaTileEntity_PotionBrewer extends GT_MetaTileEntity_BasicMachi
 
     @Override
     public int getCapacity() {
-        return 750;
+
+        switch (mTier){
+            case 1:
+                return 1000;
+            case 2:
+                return 1250;
+            case 3:
+                return 1500;
+            case 4:
+                return 1750;
+            case 5:
+                return 2000;
+            case 6:
+                return 2250;
+            case 7:
+                return 2500;
+            case 8:
+                return 2750;
+            case 9:
+                return 3000;
+            case 10:
+                return 3250;
+            case 11:
+                return 3500;
+            case 12:
+                return 3750;
+            case 13:
+                return 4000;
+            case 14:
+                return 4250;
+            case 15:
+                return 4500;
+            case 16:
+                return 4750;
+            default:
+                return 750;
+        }
+        //return 750;
     }
 }
