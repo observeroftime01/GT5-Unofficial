00b1eb4f7395581ef411bdd7fd3fa1c75cdebf3a
observeroftime02 <observeroftime@shizuki.ca>
2021-10-09 14:27:52 -0700

    Cleaned up the fuel loader class somewhat Removed no longer needed cell types from some materials.
    Signed-off-by: observeroftime02 <observeroftime@shizuki.ca>

--- src/main/java/gregtech/loaders/load/GT_FuelLoader.java
+++ src/main/java/gregtech/loaders/load/GT_FuelLoader.java
@@ -40,12 +40,11 @@ public class GT_FuelLoader implements Runnable {
         GT_Values.RA.addFuel(new ItemStack(Blocks.beacon, 1), null, Materials.NetherStar.mFuelPower * 2, Materials.NetherStar.mFuelType);
         GT_Values.RA.addFuel(GT_ModHandler.getModItem("EnderIO", "bucketRocket_fuel", 1), null, 250, 1);
 
-        // Add custom plasma fuels here, make sure the material has cells associated with it (16)
-        GT_Log.out.println("GT5u-ex: Trying to add custom plasma generator fuels.");
-        GT_Values.RA.addFuel(GTNH_ExtraMaterials.ExtremeTurbineSteel.getCells(1), Materials.Empty.getCells(1), 8192000, 4);
-        GT_Values.RA.addFuel(GTNH_ExtraMaterials.TurbineSteel.getCells(1), Materials.Empty.getCells(1), 6144000, 4);
-        GT_Log.out.println("GT5u-ex: Here's what we got so far:");
-        GT_Log.out.println("GT5u-ex: sPlasmaFuels Recipe list: " + GT_Recipe.GT_Recipe_Map.sPlasmaFuels.mRecipeList);
+        // Add custom plasma fuels here, make sure the material has cells associated with it, either molten (fuel value is for 144L, not 1000L!), or regular cells (1000L)
+        GT_Values.RA.addFuel(GT_OreDictUnificator.get(OrePrefixes.cellMolten, GTNH_ExtraMaterials.ExtremeTurbineSteel, 1), Materials.Empty.getCells(1), 1179648, 4);
+        GT_Values.RA.addFuel(GT_OreDictUnificator.get(OrePrefixes.cellMolten, GTNH_ExtraMaterials.TurbineSteel, 1), Materials.Empty.getCells(1), 884736, 4);
+        GT_Values.RA.addFuel(GT_OreDictUnificator.get(OrePrefixes.cellMolten, Materials.Mytryl, 1), Materials.Empty.getCells(1), 10240, 4);
+
 
     }
 }
